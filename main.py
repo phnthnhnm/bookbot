685a3e6fef008acd63beee69f7bf1904345d797a
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-05 16:07:30 +0700

    read file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(file):
+    file_contents = file.read()
+    return file_contents
+
+def main():
+    # Open the file in read mode
+    with open('books/frankenstein.txt', 'r') as file:
+        # Read the content of the file
+        text = get_book_text(file)
+        print(text)    
+
+main()
