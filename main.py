230912c8db332b1e712c1cfb55bf8bdf43a986cd
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-05 16:17:20 +0700

    char count dict

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_char_count
 
 def get_book_text(file):
     file_contents = file.read()
@@ -10,5 +10,7 @@ def main():
         text = get_book_text(file)
         num_words = get_num_words(text)
         print(f'{num_words} words found in the document')
+        char_count = get_char_count(text)
+        print(char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -3,3 +3,15 @@ def get_num_words(text):
     num_words = len(words)
 
     return num_words
+
+def get_char_count(text):
+    dict = {}
+
+    for char in text:
+        char = char.lower()
+        if char in dict:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+            
+    return dict
